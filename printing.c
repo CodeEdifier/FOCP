31dd61d7d5d229631f5b54943a220206f21a72af
Surbhi <surbhi@Surbhis-MacBook-Pro.local>
2019-09-27 11:36:53 +0530

    Scan integer

--- printing.c
+++ printing.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(){
+    int i;
     printf("Printing the first program\n");
     printf("run the program in the terminal\n");
+    printf("Enter in which year you read==");
+    scanf("%d",&i );
+    printf("\nWelcome in your Year no.-%d\n", i );
 
 }
