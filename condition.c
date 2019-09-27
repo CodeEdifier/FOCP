f0bed863693e62223617d9b6ab18a6df8dc36bbf
Surbhi <surbhi@Surbhis-MacBook-Pro.local>
2019-09-27 12:52:16 +0530

    if else basic

---
+++ condition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i;
+    printf("Printing the first program\n");
+    printf("run the program in the terminal\n");
+    printf("Enter in which year you read==");
+    scanf("%d",&i );
+    if(i<4){
+    printf("\nWelcome in your Year no.-%d\n", i );
+  }
+  else
+   printf("Not an eligible engineering student");
+
+}
